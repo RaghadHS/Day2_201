3ab6269b94ab4b23f721daf61b36d28f8b4a4e34
RaghadHS <raghad.alsawalhi@gmail.com>
2019-10-28 17:16:06 +0200

    First Commit - Two arrays of color are defined

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@
 
 var colors;
 colors = ['white', 'black' , 'red'];
+var secondElement = colors[1];
 
-var test = document.getElementById('colors');
-test.textContent = colors[0];
+document.write(colors[0]);
+document.write('<br>' + 'Second element of the array is' + '  ' + secondElement );
 
+var ColorArray = new Array ('Green', 'Blue' , 'Yellow');
+document.write('<br>' + '<br>' + 'Third element of the second array is' + '  ' + ColorArray[2] );
+document.write('<br>' + '<br>' + 'Fourth element of the second array is' + '  ' + ColorArray[3] );
